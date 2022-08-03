91c7782d166f5552be11437e1800b0d28abc51e1
Vikas <vikasrkad@gmail.com>
2022-08-03 23:24:35 +0530

    displayed static weather data

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import { ICurrentWeather } from '../interfaces'
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.scss']
+  styleUrls: ['./current-weather.component.scss'],
 })
 export class CurrentWeatherComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  current: ICurrentWeather
+  constructor() {
+    this.current = {
+      city: 'Bethesda',
+      country: 'US',
+      date: new Date(),
+      image: 'assets/img/sunny.png',
+      temperature: 72,
+      description: 'sunny',
+    } as ICurrentWeather
   }
 
+  ngOnInit(): void {}
 }
